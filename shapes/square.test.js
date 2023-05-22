64108659754ab86228e930621c458778821b4e63
Jmussleh <Jmussleh@gmail.comgit config --global user.name Jmussleh Jmussleh@gmail.com>
2023-05-22 08:56:08 -0400

    Added tests for all of the shapes.

---
+++ shapes/square.test.js
@@ -0,0 +1,20 @@
+const {Square} = require("./square");
+
+describe("Square", () => {
+    test("should render svg for a green polygon element", () => {
+      const expectedSvg =
+        '<rect x="90" y="40" width="120" height="120" fill="dodgerblue" />';
+      const square = new Square();
+      square.setColor("dodgerblue");
+      const actualSvg = square.render();
+      expect(actualSvg).toEqual(expectedSvg);
+    });
+    test("should accept a fillColor param", () => {
+      const expectedSvg =
+        '<rect x="90" y="40" width="120" height="120" fill="red" />';
+      const square = new Square();
+      square.setColor("red");
+      const actualSvg = square.render();
+      expect(actualSvg).toEqual(expectedSvg);
+    });
+  });
